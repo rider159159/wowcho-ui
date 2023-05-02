dbc75328963788b7f61d5305d3a5a0c459468d37
Gui <guitimliu@gmail.com>
2023-05-03 00:01:50 +0800

    feat: Footer 元件 #20

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Button: typeof import('./src/components/common/Button.vue')['default']
     DefaultLayout: typeof import('./src/components/layoutWrapper/DefaultLayout.vue')['default']
+    Footer: typeof import('./src/components/layout/Footer.vue')['default']
     Header: typeof import('./src/components/layout/Header.vue')['default']
     MemberMenu: typeof import('./src/components/common/MemberMenu.vue')['default']
     Modal: typeof import('./src/components/common/Modal.vue')['default']
