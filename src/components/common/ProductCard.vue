137a57b8fbf810b0b390927aea968cf0776cc9a1
Gui <guitimliu@gmail.com>
2023-05-14 23:39:52 +0800

    feat: 查看與選擇贊助方案切版與元件 Props #46

---
+++ src/components/common/ProductCard.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+const props = defineProps({
+  projectImage: {
+    type: String,
+    default: '/test.webp'
+  },
+  projectName: {
+    type: String,
+    default: '日本BROSKI 防水真皮背包｜不怕淋雨高質感牛皮革、包身立體不變型【嘖嘖人氣好評贊助第二彈】'
+  },
+  proposer: {
+    type: String,
+    default: '雨傘富翁'
+  },
+  proposerLink: {
+    type: String,
+    default: '#'
+  },
+  currentPrice: {
+    type: Number,
+    default: 4566000
+  },
+  targetPrice: {
+    type: Number,
+    default: 6088000
+  }
+})
+
+const currentPrice = computed(() => {
+  return props.currentPrice.toLocaleString()
+})
+
+const targetPrice = computed(() => {
+  return props.targetPrice.toLocaleString()
+})
+</script>
+
+<template>
+  <div class="md:flex gap-x-10 w-full">
+    <img
+      class="w-full md:w-96 md:h-60 mx-auto"
+      :src="projectImage"
+      :alt="props.projectName"
+    >
+    <div class="flex flex-col justify-between py-2">
+      <div class="mb-4">
+        <h1 class="text-xl font-bold mb-1">
+          {{ projectName }}
+        </h1>
+        <p class="text-gray-2">
+          提案人
+          <a class="text-brand-1" :href="proposerLink">{{ proposer }}</a>
+        </p>
+      </div>
+      <div class="flex items-center gap-x-5">
+        <ProgressBar :percent="(props.currentPrice / props.targetPrice) * 100" />
+        <div class="text-gray-2">
+          <p class="text-sm mb-2">目標 NT {{ targetPrice }}</p>
+          <p class="font-bold text-3xl">NT {{ currentPrice }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/proposal/plan.vue
+++ src/views/proposal/plan.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-  <div>
-    <h1>查看贊助方案</h1>
+  <!-- <h1>查看贊助方案</h1> -->
+  <div class="container pt-10 mx-auto">
+    <ProductCard class="mb-10" />
+    <PlanList
+      class="mb-10"
+      :data="[{}, {}, {}, {}]"
+    />
   </div>
 </template>
