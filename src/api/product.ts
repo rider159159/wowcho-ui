1d5a56a2b1ef714da97cd2857804f8418c11697d
Ryder <rider159159@gmail.com>
2023-05-28 18:24:35 +0800

    fix: 移除不必要程式碼、添加 Noto 字體

--- src/api/product.ts
+++
@@ -1,6 +0,0 @@
-import { http } from '@/utils/axios'
-import { ApiResponse } from '@/interface'
-
-export const fetchProduct = {
-  getProducts: (params?:any):Promise<ApiResponse> => http.httpGet('/proposal', { params })
-}
