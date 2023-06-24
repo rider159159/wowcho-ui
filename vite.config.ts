d422a791a694329ff6e68489e098d834b031dd84
Ryder <rider159159@gmail.com>
2023-06-24 16:24:13 +0800

    fix: fix vite base

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ command, mode }) => {
   console.log('command', command)
   console.log('mode', mode)
   return {
-    base: './',
+    base: '/',
     plugins: [
       vue(),
       AutoImport({
